639150cba088fd65d9cf5be3ea4a37087d4a18cc
Youssef Sameh <youssefsamehh01@gmail.com>
2023-12-12 08:55:56 +0200

    Uploading the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./html/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#202124",
+        sec:"#303134",
+        thin: "rgb(255 255 255 / 20%)"
+      },
+    },
+  },
+  plugins: [],
+}
+
